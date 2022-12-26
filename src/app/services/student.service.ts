e14b6b75a74ef33356ff8bb2bf4127b4cdee1220
Jonatas Santos <jonatas.santos@produsti.com.br>
2022-12-26 14:26:50 -0300

    ✨ Add save, update and delete methods to student service

--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -8,16 +8,27 @@ import { Student } from '../models/student';
   providedIn: 'root'
 })
 export class StudentService {
-  protected serviceUrl = environment.serviceUrl;
-  protected webApiSuffix: string = '/api/Student';
+  protected serviceUrl = `${environment.serviceUrl}/api/Student`;
 
   constructor(private http: HttpClient) { }
 
   public getAll(): Observable<Array<Student>> {
-    return this.http.get<Array<Student>>(`${this.serviceUrl}${this.webApiSuffix}`);
+    return this.http.get<Array<Student>>(`${this.serviceUrl}`);
   }
 
   public getById(id: number): Observable<Student> {
-    return this.http.get<Student>(`${this.serviceUrl}${this.webApiSuffix}/${id}`);
+    return this.http.get<Student>(`${this.serviceUrl}/${id}`);
+  }
+
+  public save(student: Student) {
+    return this.http.post(`${this.serviceUrl}`, student);
+  }
+
+  public update(student: Student) {
+    return this.http.put(`${this.serviceUrl}/${student.id}`, student);
+  }
+
+  public delete(id: number) {
+    return this.http.delete(`${this.serviceUrl}/${id}`);
   }
 }
