28c3446249d50eb3108c129270e2df0b675830eb
Jonatas Santos <jonatas.santos@produsti.com.br>
2022-12-27 13:55:38 -0300

    ♻️ Change property type

--- src/app/models/student.ts
+++ src/app/models/student.ts
@@ -1,6 +1,13 @@
 export class Student {
+    constructor() {
+        this.id = 0;
+        this.name = '';
+        this.lastName = '';
+        this.phone = ''
+    }
+
     id: number;
     name: string;
     lastName: string;
-    phone: number
+    phone: string
 }
